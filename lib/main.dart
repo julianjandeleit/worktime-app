9109ac4a09015841b2896fd3288881dcc3f0d982
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2023-08-20 17:29:13 +0200

    basic supabase ui auth

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:provider/provider.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:work_time_app/firebase_options.dart';
 import 'package:work_time_app/models/WorkSession.dart';
+import 'package:work_time_app/sup_auth.dart';
 import 'package:work_time_app/viewmodels/ProjectViewModel.dart';
 import 'models/Project.dart';
 import 'models/basic_list.dart';
@@ -12,20 +13,27 @@ import 'userDataProvider.dart';
 import 'login.dart';
 import 'viewmodels/WorkSessionViewModel.dart';
 import 'workspace.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChannels.lifecycle.setMessageHandler((message) async {
-    if (message == AppLifecycleState.resumed.toString()) {
-      // Set the log level to warning or error
-      await logWarningOrError();
-    }
-  });
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-  final GoogleSignIn googleSignIn = GoogleSignIn(
-      scopes: ['email'],
-      clientId: DefaultFirebaseOptions.currentPlatform.appId);
+  await Supabase.initialize(
+    url: "https://wudetldwulxvzcjnyjcz.supabase.co",
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1ZGV0bGR3dWx4dnpjam55amN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTI1NDA4MjAsImV4cCI6MjAwODExNjgyMH0.UhJ-Cq55JIN87E5tlE_r7V-woRNKA8ZCaEzmywzupqQ",
+  );
+  // SystemChannels.lifecycle.setMessageHandler((message) async {
+  //   if (message == AppLifecycleState.resumed.toString()) {
+  //     // Set the log level to warning or error
+  //     await logWarningOrError();
+  //   }
+  // });
+  //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // final GoogleSignIn googleSignIn = GoogleSignIn(
+  //     scopes: ['email'],
+  //     clientId: DefaultFirebaseOptions.currentPlatform.appId);
 
   runApp(
     MultiProvider(
@@ -34,15 +42,13 @@ void main() async {
         ChangeNotifierProvider(create: (_) => ProjectViewModel()),
         ChangeNotifierProvider(create: (_) => WorkSessionViewModel())
       ],
-      child: WorkTimeApp(googleSignIn: googleSignIn),
+      child: WorkTimeApp(),
     ),
   );
 }
 
 class WorkTimeApp extends StatelessWidget {
-  final GoogleSignIn googleSignIn;
-
-  WorkTimeApp({required this.googleSignIn});
+  WorkTimeApp();
 
   @override
   Widget build(BuildContext context) {
@@ -51,10 +57,10 @@ class WorkTimeApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/main',
+      initialRoute: '/login',
       routes: {
-        '/': (context) => AuthScreen(),
-        '/main': (context) => Workspace(),
+        '/login': (context) => SupAuth(),
+        '/': (context) => WorkTimeApp(),
       },
     );
   }
---
+++ lib/sup_auth.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
+
+class SupAuth extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final supabase = Supabase.instance.client;
+    return Scaffold(
+        appBar: AppBar(title: Text('Authentication')),
+        body: Center(
+            child: Column(
+          children: [
+            Text(supabase.auth.currentUser?.email ?? "no user"),
+            SupaSocialsAuth(
+              socialProviders: [SocialProviders.google],
+              colored: true,
+              showSuccessSnackBar: true,
+              redirectUrl: null,
+              onSuccess: (p0) {
+                print("succeess");
+              },
+              onError: (error) {
+                print("error");
+              },
+            ),
+          ],
+        )));
+  }
+}
