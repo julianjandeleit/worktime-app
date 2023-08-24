8dee38cdbe13314337f89b236b862c2eea5fc0d1
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2023-08-24 22:02:34 +0200

    fix new issues

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -22,8 +22,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9c2f65b8d74e9ad30b667689f90e978f",
-"/": "9c2f65b8d74e9ad30b667689f90e978f",
+"index.html": "186118cfd2fe0b0ca714bca4768e4e24",
+"/": "186118cfd2fe0b0ca714bca4768e4e24",
 "main.dart.js": "55c8c71698dd4737414ad02c4ba3c046",
 "manifest.json": "da6c4e36b2e874fe1d50e2ed7b981330",
 "version.json": "5dd752c88bc0f1b8fc88a6f87171d623"
