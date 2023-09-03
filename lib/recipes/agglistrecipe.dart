d53db53cc6d002820a51c0ac15f304f4a1aa929b
Julian Jandeleit <54635597+julianjandeleit@users.noreply.github.com>
2023-09-03 17:18:00 +0200

    recipe aggregations

---
+++ lib/recipes/agglistrecipe.dart
@@ -0,0 +1,154 @@
+import 'dart:math';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:work_time_app/recipes/listrecipe.dart';
+import 'package:work_time_app/util/recipeable.dart';
+
+import 'package:flutter/material.dart';
+import 'package:work_time_app/util/recipeable.dart';
+
+class AggListRecipe<T extends Recipeable> extends StatelessWidget {
+  final List<T> itemList;
+  final void Function(List<T> items) onChanged;
+  final int? selectedIndex;
+  final void Function(int index)? onItemSelect;
+
+  final void Function()? onAdd; // Add this property for the add button
+
+  AggListRecipe({
+    required this.itemList,
+    required this.onChanged,
+    this.selectedIndex,
+    this.onItemSelect,
+    this.onAdd, // Initialize the property
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final ScrollController _scrollController = ScrollController();
+    return LayoutBuilder(builder: (context, constraints) {
+      return Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          IconButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text('Edit List'),
+                      content: SizedBox(
+                          height: 400,
+                          width: 500,
+                          child: ListRecipe(
+                            itemList: itemList,
+                            onChanged: (items) {
+                              onChanged(items);
+                              Navigator.pop(context);
+                            },
+                            onItemSelect: onItemSelect != null
+                                ? (index) {
+                                    onItemSelect!(index);
+                                    Navigator.pop(context);
+                                  }
+                                : null,
+                            selectedIndex: selectedIndex,
+                            onAdd: onAdd != null
+                                ? () {
+                                    onAdd!();
+                                    Navigator.pop(context);
+                                  }
+                                : null,
+                          )),
+                    );
+                  },
+                );
+              },
+              icon: const Icon(Icons.edit)),
+          Expanded(
+            child: Container(
+              decoration:
+                  const BoxDecoration(border: Border(left: BorderSide())),
+              padding: const EdgeInsets.all(15),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Listener(
+                  onPointerSignal: (event) {
+                    if (event is PointerScrollEvent) {
+                      final offset = event.scrollDelta.direction *
+                          event.scrollDelta.distance *
+                          0.25;
+                      _scrollController
+                          .jumpTo(_scrollController.offset + offset);
+                    }
+                  },
+                  child: Scrollbar(
+                    controller: _scrollController,
+                    child: ListView.separated(
+                      controller: _scrollController,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: itemList
+                          .length, // Add an extra item for the Add button
+                      itemBuilder: (context, index) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: MouseRegion(
+                              cursor:
+                                  onItemSelect != null && index != selectedIndex
+                                      ? SystemMouseCursors.click
+                                      : MouseCursor.defer,
+                              child: GestureDetector(
+                                onTap: onItemSelect != null &&
+                                        index != selectedIndex
+                                    ? () {
+                                        onItemSelect!(index);
+                                      }
+                                    : null,
+                                child: Container(
+                                  padding: const EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                    color: selectedIndex == index
+                                        ? Colors.blue.withOpacity(0.1)
+                                        : Colors.grey.withOpacity(0.1),
+                                    borderRadius: BorderRadius.circular(8),
+                                  ),
+                                  width: max(constraints.maxWidth / 4.0,
+                                      constraints.minWidth),
+                                  child: itemList[index].buildAggregation(
+                                    onChanged: (p0) {
+                                      final copiedList = [...itemList];
+                                      copiedList[index] = p0 as T;
+                                      onChanged(copiedList);
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      separatorBuilder: (BuildContext context, int index) =>
+                          Container(
+                        margin: const EdgeInsets.all(15),
+                        child: Divider(
+                          color: Colors.black.withOpacity(0.75),
+                          endIndent: 15,
+                          thickness: 3,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      );
+    });
+  }
+}
---
+++ lib/recipes/aggregationrecipe.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class AggregationRecipe extends StatefulWidget {
+  final String heading;
+  final List<String> names;
+  final List<String> values;
+  //final void Function(String) onChanged;
+  final Widget Function()? buildDetails;
+
+  AggregationRecipe(
+      {required this.heading,
+      required this.names,
+      required this.values,
+      this.buildDetails});
+
+  @override
+  _AggregationRecipeState createState() => _AggregationRecipeState();
+}
+
+Iterable<T> zip_alternating<T>(Iterable<T> a, Iterable<T> b) sync* {
+  final ita = a.iterator;
+  final itb = b.iterator;
+  bool hasa, hasb;
+  while ((hasa = ita.moveNext()) | (hasb = itb.moveNext())) {
+    if (hasa) yield ita.current;
+    if (hasb) yield itb.current;
+  }
+}
+
+class _AggregationRecipeState extends State<AggregationRecipe> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (widget.names.length != widget.values.length) {
+      throw Exception("same number of names and values required");
+    }
+
+    var names = widget.names.map(
+      (e) => Align(
+        alignment: Alignment.center,
+        child: Text(
+          e,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+
+    var values = widget.values.map(
+      (e) => Align(
+        alignment: Alignment.center,
+        child: Text(
+          e,
+          style: TextStyle(fontWeight: FontWeight.normal),
+        ),
+      ),
+    );
+
+    final widgets = zip_alternating(names, values).toList();
+
+    return MouseRegion(
+      cursor: widget.buildDetails != null
+          ? SystemMouseCursors.click
+          : MouseCursor.defer,
+      child: GestureDetector(
+        onLongPress: widget.buildDetails != null
+            ? () {
+                //TODO: how to make dialog keep changes
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      title: Text(
+                          'Edit ${widget.heading} (does not reflect changes)'),
+                      content: SizedBox(
+                          height: 400,
+                          width: 500,
+                          child: widget.buildDetails!.call()),
+                    );
+                  },
+                );
+              }
+            : null,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Text(widget.heading),
+            ),
+            AlignedGridView.count(
+              shrinkWrap: true,
+              itemCount: widgets.length,
+              crossAxisCount: 2,
+              mainAxisSpacing: 4,
+              crossAxisSpacing: 4,
+              itemBuilder: (context, index) {
+                return widgets[index];
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
